e43e8ac8eb5865ae01362cca874c16cccb3142e2
PavKmiec <pawelkmiec@gmail.com>
2020-11-07 15:37:10 +0000

    fixes face detection; implemented face tracking and drone take off

--- faceTracking.py
+++ faceTracking.py
@@ -1,25 +1,47 @@
 from utils import *
 
+# params
+# w,h
+width = 360
+height = 240
+
+# set pid control values
+pid = [0.5, 0.5, 0]
+pErr = 0
+
+startCounter = 0 # test for no flight = 1, for flight = 0
+
+dim = (width, height)
+
 
 # init drone
 myDrone = initTello()
 
-### getting image from tello
-
-#declare width and height for image
-w, h = 360, 240
 
 
+# main loop
 while True:
-    #get frame
+    # Take off, fly
+    if startCounter == 0:
+        myDrone.takeoff()
+        startCounter = 1
+
+    #get frame from tello
     img = telloGetFrame(myDrone, dim)
 
-    #call find face
-    img = findFace(img)
+    # find face
+    img_from_tello, inf = findFace(img)
+    
+    # track face 
+    pErr = trackFace(myDrone, inf, width, pid, pErr)
 
-    cv2.imshow('img', img)
+    
+    #print(inf[0][0])
 
-    # stop the drone on q kay (safety measure)
+    # display
+    cv2.imshow('Image', img)
+
+    #stop the drone on q kay (safety measure)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         myDrone.land()
         break
@@ -30,4 +52,3 @@ while True:
 
 
 
-#track face
